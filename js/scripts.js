0cd6f4acb7893911a7cee1ce0233fa40da407c4b
g00gol <38109082+g00gol@users.noreply.github.com>
2020-09-24 08:50:46 -0500

    Upload

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// ========== AOS ===========
+$(function () {
+	AOS.init();
+});
+
+//=========== Footer ============
+/* $(window).scroll(function () {
+	if ($(window).scrollTop() + $(window).height() == $(document).height()) {
+		document.getElementById("footer").style.position = "fixed";
+	}
+}); */
