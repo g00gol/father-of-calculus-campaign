556a5045c4c4b2a10ebb1360c05fa56ef93c1121
devision <38109082+g00gol@users.noreply.github.com>
2020-09-30 16:27:27 -0500

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,3 @@
 $(function () {
 	AOS.init();
 });
-
-//=========== Footer ============
-/* $(window).scroll(function () {
-	if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-		document.getElementById("footer").style.position = "fixed";
-	}
-}); */
