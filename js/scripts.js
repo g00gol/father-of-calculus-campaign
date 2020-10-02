e9ef7f29c285d4662f5806c91dc08487b9849bdf
g00gol <38109082+g00gol@users.noreply.github.com>
2020-10-01 19:06:58 -0500

    Text, HTML, CSS, Javascript

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,97 @@
 $(function () {
 	AOS.init();
 });
+
+// ============ Bar Graph =============
+
+// =========== Isaac Graph ==========
+function isaacGraph() {
+	document.getElementById("vote-text-cont").style.opacity = "0";
+	$("#vote-text-cont").css("z-index", -2);
+
+	$("#isaac-bar").animate(
+		{
+			height: "55%",
+		},
+		{
+			delay: 500,
+			duration: 4000,
+		}
+	);
+	$("#wilhelm-bar").animate(
+		{
+			height: "45%",
+		},
+		{
+			delay: 500,
+			duration: 4000,
+		}
+	);
+
+	$("#count").text("55");
+
+	$("#count").each(function () {
+		$(this)
+			.prop("Counter", 0)
+			.animate(
+				{
+					Counter: $(this).text(),
+				},
+				{
+					duration: 4000,
+					easing: "swing",
+					step: function (now) {
+						$(this).text(Math.ceil(now));
+					},
+				}
+			);
+	});
+
+	$("#isaac-img").delay(4000).animate({ width: "100px" }, 1000);
+}
+
+// ============ Wilhelm Graph =============
+function wilhelmGraph() {
+	document.getElementById("vote-text-cont").style.opacity = "0";
+	$("#vote-text-cont").css("z-index", -2);
+
+	$("#wilhelm-bar").animate(
+		{
+			height: "55%",
+		},
+		{
+			delay: 500,
+			duration: 4000,
+		}
+	);
+	$("#wilhelm-bar").animate(
+		{
+			height: "45%",
+		},
+		{
+			delay: 500,
+			duration: 4000,
+		}
+	);
+
+	$("#count").text("55");
+
+	$("#count").each(function () {
+		$(this)
+			.prop("Counter", 0)
+			.animate(
+				{
+					Counter: $(this).text(),
+				},
+				{
+					duration: 4000,
+					easing: "swing",
+					step: function (now) {
+						$(this).text(Math.ceil(now));
+					},
+				}
+			);
+	});
+
+	$("#wilhelm-img").delay(4000).animate({ width: "100px" }, 1000);
+}
